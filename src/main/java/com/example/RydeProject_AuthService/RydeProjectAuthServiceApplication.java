7b13d58511899cc56cb9369a774ded9a59f4606d
Tohid Naikwadi <naikwadimdtohid@gmail.com>
2025-10-19 15:12:31 +0530

    Initial setup for microservice

---
+++ src/main/java/com/example/RydeProject_AuthService/RydeProjectAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.RydeProject_AuthService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RydeProjectAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RydeProjectAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RydeProject_AuthService/RydeProjectAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RydeProject_AuthService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RydeProjectAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
